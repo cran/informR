d458c9075bbceaebb77509306ea2f29522ea5312
Christopher Steven Marcum <cmarcum@uci.edu>
2012-03-20 00:00:00 +0000

    version 1.0.04

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
 #
 # zzz.R
 #
-# Last Modified 12/21/11
+# Last Modified 03/20/12
 # Licensed under the GNU General Public License version 2 (June, 1991)
 #
 # Part of the R/informR package
@@ -11,8 +11,15 @@
 #
 ######################################################################
 
-.First.lib <- function(lib, pkg){
-    print(packageDescription('informR'))
-    cat(paste("copyright (c) 2010, Christopher S. Marcum, RAND Corporation\n",sep=""))
-    cat('Type help(package="informR") to get started.\n')
+.onLoad <- function(lib, pkg){
+    abt<-packageDescription('informR')
+packageStartupMessage(
+    c("\n\t",abt$Title,"\n"),
+    c("Version: ",abt$Version,"\n"),
+    c("Compiled:",abt$Built,"\n\n"),
+    c(paste("copyright (c) 2010, Christopher S. Marcum\n",sep="")),
+    c('Type help(package="informR") to get started.\n'))
 }
+
+
+
